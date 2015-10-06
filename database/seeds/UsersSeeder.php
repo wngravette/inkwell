7273da62876eca385ffe9d9a6da053ba57ff3162
William Naughton-Gravette <william@willng.me>
2015-10-06 23:52:49 +1100

    testting seeds

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'first_name' => str_random(10),
+            'last_name' => str_random(20),
+            'email' => str_random(10).'@gmail.com',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
