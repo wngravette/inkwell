834ddd5b400078a105df2b84e88471981781810a
William‌‌ Naughton-Gravette <william@willng.me>
2015-10-11 19:41:48 +1100

    Update UsersSeeder.php

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -14,7 +14,7 @@ class UsersSeeder extends Seeder
         DB::table('users')->insert([
             'first_name' => str_random(10),
             'last_name' => str_random(20),
-            'email' => str_random(10).'@gmail.com',
+            'email' => str_random(10).'@email.com',
             'password' => bcrypt('secret'),
         ]);
     }
