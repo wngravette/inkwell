ab8a9ef84d5e0e92b2e9f917ab7c6f0835c310b8
William Naughton-Gravette <william@willng.me>
2015-10-16 15:58:20 +1100

    set up mail and queues

--- config/mail.php
+++ config/mail.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'mail'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'noreply@journal.willng.me', 'name' => 'Inkwell'],
 
     /*
     |--------------------------------------------------------------------------
