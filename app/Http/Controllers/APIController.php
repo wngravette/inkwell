b8dcf756369e76bc91361419b5d00f186b29a6bb
William Naughton-Gravette <william@willng.me>
2015-10-26 20:16:01 +1100

    fix contribs bug
    not really relevant for production, but still annoying

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -20,7 +20,7 @@ class APIController extends Controller
             $value = 1;
 
             foreach ($entries as $entry) {
-                $timestamp = date_timestamp_get($entry->updated_at);
+                $timestamp = date_timestamp_get(Carbon::createFromFormat('Y-m-d', $entry->entry_date));
                 $contribs[$timestamp] = $value;
             }
 
