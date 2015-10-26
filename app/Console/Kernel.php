decdb157bc8d1179fc1a3dc615cf7c3a457ca245
William Naughton-Gravette <william@willng.me>
2015-10-26 13:42:11 +1100

    fix Kernal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,10 +6,13 @@ use App\Entry;
 use App\Jobs\GenerateEntryStats;
 use Carbon\Carbon;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    use DispatchesJobs;
+
     /**
      * The Artisan commands provided by your application.
      *
@@ -41,8 +44,10 @@ class Kernel extends ConsoleKernel
             $yesterday = Carbon::yesterday()->format('Y-m-d');
             $entries = Entry::where('entry_date', "<=", $yesterday)->get();
             foreach ($entries as $entry) {
+                $entry->is_signed = 1;
+                $entry->save();
                 $this->dispatch(new GenerateEntryStats($entry));
             }
-        })->daily();
+        })->hourly();
     }
 }
