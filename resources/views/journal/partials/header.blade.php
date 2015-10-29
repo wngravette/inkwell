47169661becf031fd344f9168a5e6c1b37ae741a
William Naughton-Gravette <william@willng.me>
2015-10-29 20:04:17 +1100

    updated nav menu

--- resources/views/journal/partials/header.blade.php
+++ resources/views/journal/partials/header.blade.php
@@ -3,6 +3,6 @@
         <h1 class="logo">{{config('app.app_name')}}<span class="sep">/</span><span class="menu_btn">{{$page_name}} <i class="fa fa-chevron-down menu_caret"></i></span></h1>
     </div>
     <div class="one-forth column right">
-        <p class="header">Welcome, {{Auth::user()->first_name}}<span><i class="fa fa-cog"></i><a href="/auth/logout"><i class="fa fa-sign-out"></i></a></span></p>
+        <p class="header">Welcome, {{Auth::user()->first_name}}<span><a href="/journal/preferences"><i class="fa fa-cog"></i></a><a href="/auth/logout"><i class="fa fa-sign-out"></i></a></span></p>
     </div>
 </div>
